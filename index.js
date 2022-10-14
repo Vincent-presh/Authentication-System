e0a9f80a5feafb77c15058fc5429c552e2855f6d
Vincentpresh <5tcentunary@gmail.com>
2022-10-14 08:14:53 +0100

    Add Javscript and Validate funtion to Login Form

---
+++ index.js
@@ -0,0 +1,6 @@
+var emailInput = document.getElementById("email");
+var passwordInput = document.getElementById("password");
+
+function validate() {
+  alert("Email: " + emailInput.value + " Password: " + passwordInput.value);
+}
