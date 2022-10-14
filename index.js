94fb1252b2c4b3373662b0b6f025923e9eae87df
Vincentpresh <5tcentunary@gmail.com>
2022-10-14 13:29:55 +0100

    Week  6: Call Login Endpoint with Fetch and get Response

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+// Get DOM elements for the INPUT elements
 var emailInput = document.getElementById("email");
 var passwordInput = document.getElementById("password");
 
-function validate() {
+//asynchronous function to make request to server.
+async function validate() {
   alert("Email: " + emailInput.value + " Password: " + passwordInput.value);
+  //Fetch method post request
+  await fetch("http://localhost:3000/login" {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    body: JSON.stringify({
+      // "email": emailInput.value
+      //  "password": passwordInput
+      email: emailInput.value,
+      password: passwordInput.value,
+    })
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      //Alert Response data
+      alert(data);
+    });
 }
